e12900ac44f5ce847472bf152e8353c4840d807b
DurantJr <whod0977@gmail.com>
2025-11-07 10:12:26 +0000

    fix backend

--- places_to_go/models.py
+++ places_to_go/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+import os
+
+static_places_storage = FileSystemStorage(
+    location=os.path.join(settings.BASE_DIR, "places_to_go", "static", "places"),
+    base_url="/static/places/"
+)
 
 class Place(models.Model):
     name = models.CharField(max_length=100)
@@ -11,7 +19,7 @@ class Place(models.Model):
     title = models.CharField(max_length=100, blank=True, null=True)
     site = models.URLField(blank=True, null=True)
     menu = models.URLField(blank=True, null=True)
-    image = models.ImageField(upload_to='places/', blank=True, null=True)
+    image = models.ImageField(storage=static_places_storage, upload_to='places', blank=True, null=True)
  
     def __str__(self):
         return f"{self.name} ({self.place_type})"
